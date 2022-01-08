4d0f2dd25438b83db38485c8e065e08927d8e446
Ryan Aurelio <s8ryaure@stud.uni-saarland.de>
2022-01-09 00:02:48 +0100

    Basic movement and skeleton for map

---
+++ map.py
@@ -0,0 +1,14 @@
+class Map:
+    map = []
+
+    def __init__(self, width, height):
+        if ()
+        self.map += ["x"]
+
+    def show(self):
+        pass
+
+    def set_grid(self, status):
+        pass
+
+

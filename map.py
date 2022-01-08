c2012d950e77d1e508b2ea2be76dd27fe99e8076
Ryan Aurelio <s8ryaure@stud.uni-saarland.de>
2022-01-09 00:18:49 +0100

    init and repr for Map

--- map.py
+++ map.py
@@ -1,14 +1,21 @@
 class Map:
-    map = []
+    def __init__(self, height, width):
+        if height < 3 or width < 3:
+            raise ValueError("Minimum size 3x3.")
+        self.grid = []
+        self.grid.append(["x"] * width)
+        for _ in range(height-2):
+            self.grid.append(["x"] + ["."]*(width-2) + ["x"])
+        self.grid.append(["x"] * width)
 
-    def __init__(self, width, height):
-        if ()
-        self.map += ["x"]
-
-    def show(self):
-        pass
+    def __repr__(self):
+        repr = ""
+        for row in self.grid:
+            repr += "".join(row) + "\n"
+        return repr
 
     def set_grid(self, status):
         pass
 
-
+m = Map(10, 10)
+print(m)

dfe2fc71603255e42012a0806c41bcb6529b434a
Ryan Aurelio <s8ryaure@stud.uni-saarland.de>
2022-01-17 05:51:02 +0100

    add main class and include example to run the integrated program

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from contextlib import closing
+from map import Map
+from pathfinder import explore
 from robot import Robot
 
+# Specify map
+print("Map:")
+m = Map(6, 6)
+m.set_grid(3, 2, 'x')
+m.set_grid(3, 3, 'x')
+m.set_grid(3, 4, 'x')
+print(m)
+
+# Explore map sequence
+seq = explore(m, 4, 1)
+print(seq)
+
+# Execute move command in robot
 with closing(Robot(6)) as r:
-    r.move_sequence("WDSASDWAASDW")
+    r.move_sequence(seq)

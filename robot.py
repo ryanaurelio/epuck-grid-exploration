c84301bad558dea66075073e3fcdce24ecd00120
Ryan Aurelio <s8ryaure@stud.uni-saarland.de>
2022-01-17 03:30:13 +0100

    Update docstring for robot class

--- robot.py
+++ robot.py
@@ -1,8 +1,6 @@
 import serial
 import time
 
-from contextlib import closing
-
 # Speed
 SPEED = 550
 
@@ -13,21 +11,29 @@ SOUTH = 2
 WEST = 3
 
 class Robot:
-    """Robot class"""
-    def __init__(self, com_number):
-        """Initialize"""
+
+    def __init__(self, com_number: int) -> None:
+        """
+        Initialize Robot class
+        :param com_number: COM port to communicate with e-puck
+        """
         self.ser = serial.Serial(f'COM{com_number}', 115200, timeout=0)
         self.direction = NORTH
+
         self.status = 'IDLE'
         self.home_coordinate = (0, 0)
         self.old_coordinate = (0, 0)
 
-    def close(self):
-        """Close connection with robot"""
+    def close(self) -> None:
+        """
+        Close the connection with e-puck
+        """
         self.ser.close()
 
-    def step_forward(self):
-        """Step forward"""
+    def step_forward(self) -> None:
+        """
+        Move e-puck forward by one step
+        """
         self.ser.write(f'D,{SPEED},{SPEED}\r\n'.encode())
         time.sleep(1)
         self.ser.write(bytes(b'D,0,0\r\n'))
@@ -35,8 +41,10 @@ class Robot:
         # Delay
         time.sleep(0.2)
     
-    def step_backward(self):
-        """Step backward"""
+    def step_backward(self) -> None:
+        """
+        Move e-puck backward by one step
+        """
         self.ser.write(f'D,{-SPEED},{-SPEED}\r\n'.encode())
         time.sleep(1)
         self.ser.write(bytes(b'D,0,0\r\n'))
@@ -44,8 +52,10 @@ class Robot:
         # Delay
         time.sleep(0.2)
     
-    def turn_left(self):
-        """Turn left"""
+    def turn_left(self) -> None:
+        """
+        Turn e-puck by 90° anti-clockwise
+        """
         self.ser.write(f'D,{-SPEED},{SPEED}\r\n'.encode())
         time.sleep(0.58177)
         self.ser.write(bytes(b'D,0,0\r\n'))
@@ -57,8 +67,10 @@ class Robot:
         # Delay
         time.sleep(0.2)
 
-    def turn_right(self):
-        """Turn right"""
+    def turn_right(self) -> None:
+        """
+        Turn e-puck by 90° clockwise
+        """
         self.ser.write(f'D,{SPEED},{-SPEED}\r\n'.encode())
         time.sleep(0.58177)
         self.ser.write(bytes(b'D,0,0\r\n'))
@@ -70,8 +82,11 @@ class Robot:
         # Delay
         time.sleep(0.2)
 
-    def update_direction(self, new_direction):
-        """Fix direction of robot"""
+    def update_direction(self, new_direction: int) -> None:
+        """
+        Update the direction to where the e-puck will be facing
+        :param new_direction: New facing direction for the e-puck
+        """
         if self.direction == new_direction:
             return
         elif (self.direction-1)%4 == new_direction:
@@ -82,28 +97,39 @@ class Robot:
             self.turn_left()
             self.turn_left()
     
-    def step_north(self):
-        """Step north"""
+    def step_north(self) -> None:
+        """
+        Move e-puck to the north by one step
+        """
         self.update_direction(NORTH)
         self.step_forward()
     
-    def step_east(self):
-        """Step east"""
+    def step_east(self) -> None:
+        """
+        Move e-puck to the east by one step
+        """
         self.update_direction(EAST)
         self.step_forward()
     
-    def step_south(self):
-        """Step south"""
+    def step_south(self) -> None:
+        """
+        Move e-puck to the south by one step
+        """
         self.update_direction(SOUTH)
         self.step_forward()
     
-    def step_west(self):
-        """Step west"""
+    def step_west(self) -> None:
+        """
+        Move e-puck to the west by one step
+        """
         self.update_direction(WEST)
         self.step_forward()
     
-    def move_sequence(self, seq):
-        """Parse WASD sequence into corresponding move command"""
+    def move_sequence(self, seq: str) -> None:
+        """
+        Parse WASD sequence into corresponding move command
+        :param seq: WASD sequence to move e-puck
+        """
         for c in seq:
             match c:
                 case 'W':
@@ -114,6 +140,3 @@ class Robot:
                     self.step_south()
                 case 'D':
                     self.step_east()
-
-with closing(Robot(6)) as r:
-    r.move_sequence("WDSASDWAASDW")
